956807375180a1073dc73f1429f004bd8fba3a5a
camila <226563668+CamilaLima03@users.noreply.github.com>
2025-08-25 15:02:08 -0300

    anotações treinamento

---
+++ aula_07-25_08/exercicio03.py
@@ -0,0 +1,9 @@
+#menu de comando para robôs
+#1-robô em posição inicial
+#2- avançar-recuar-status-desligar
+#3- pedir escolha comando
+#4- conf escolha execute avançar(adicionar posição), recuar(subtratir pos), status(mostrar pos) e desligar (encerra)
+#5- o menu continua aparecendo até desligar
+#6- dig comando errado = erro
+
+
