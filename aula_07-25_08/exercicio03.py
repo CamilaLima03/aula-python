47a1a84caebe5d87d1ee51f0851764192a95fbb0
camila <226563668+CamilaLima03@users.noreply.github.com>
2025-08-25 15:33:53 -0300

    robo

--- aula_07-25_08/exercicio03.py
+++ aula_07-25_08/exercicio03.py
@@ -7,3 +7,23 @@
 #6- dig comando errado = erro
 
 
+posicao = 0
+
+while True:
+    opcao = input( "Escolha uma ação\n" \
+                  "1 - Avançar.\n" \
+                  "2 - Recuar.\n" \
+                  "3 - Status. \n" \
+                  "4 - Desligar. \n")
+    if opcao == "1":
+        posicao += 1
+    elif opcao == "2": 
+        posicao - 1
+    elif opcao == "3":
+        print (f"Local robõ é {posicao}")   
+    elif opcao == "6":
+        print (" Programa encerrado. ")
+        break
+    else:
+        print("Opção incorreta.")           
+          
