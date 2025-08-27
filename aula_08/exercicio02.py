f63c22009076688bb09bf8945a634b3b486739a6
CamilaLima03 <226563668+CamilaLima03@users.noreply.github.com>
2025-08-27 14:54:49 -0300

    verificar digitos iguais

--- aula_08/exercicio02.py
+++ aula_08/exercicio02.py
@@ -4,6 +4,16 @@
 #se for válido formatar padrão
 #imprimir o numero formatado ou mensagem de erro
 
-numero = int(input("Digite seu telefone com DDD: "))
+numero = input("Digite seu telefone com DDD: ")
+valido = True
 
-
+for c in numero:
+    contador_repetidos = 0
+    for d in numero:
+        print (f"num c {c} num {d} são iguais? {c==d}")
+        if c==d:
+            contador_repetidos+=1
+        if contador_repetidos >=3:
+            print("Número inválido!")
+            valido = False
+            break               
