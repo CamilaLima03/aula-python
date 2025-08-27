5159b114cfc61dc369c40f950d880f7a7fe9df5f
CamilaLima03 <mylikascl@gmail.com>
2025-08-27 14:18:21 -0300

    exercicio2

---
+++ aula_08/exercicio02.py
@@ -0,0 +1,9 @@
+#validaçãotelefone
+#criar programa que recebe 11digitos
+#tornar invalido se repetir 3x
+#se for válido formatar padrão
+#imprimir o numero formatado ou mensagem de erro
+
+numero = int(input("Digite seu telefone com DDD: "))
+ print = numero
+
