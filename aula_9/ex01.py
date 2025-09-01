da17f8d88e0d8e36e9dee89d1890c2f6eb8f7a63
mk <226563668+CamilaLima03@users.noreply.github.com>
2025-09-01 14:11:01 -0300

    intro 2

---
+++ aula_9/ex01.py
@@ -0,0 +1,11 @@
+#crie um programa que conte quantas vezes um num aparece
+#entrada: uma lista de num a ser procurado
+#saida: quantas vezes o num aparece
+
+
+numeros = [1, 5, 2, 8 , 5, 3, 5]
+numero_procurado = 5
+contador = 0
+
+for numero in numeros:
+    
---
+++ aula_9/ex02.py
@@ -0,0 +1,7 @@
+#comparar duas listas e criar uma terceira apenas com  itens em comum
+#entrada duas listas
+#saída uma nova lista
+
+#ex: 
+#lista 01: [vermelho,azul,verde, amarelo]
+#lista02: [verde, roxo, azul, preto]
