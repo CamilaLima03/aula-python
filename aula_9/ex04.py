b8d86dc1d56a6d38116baaab1e69dc8a3ffa0074
mk <226563668+CamilaLima03@users.noreply.github.com>
2025-09-01 15:54:55 -0300

    exercício 04

---
+++ aula_9/ex04.py
@@ -0,0 +1,9 @@
+#coletor de dados robustos
+#crie um programa que colete números de usuarios e os armazene em uma lista. O programa deve continuar pedindo números até que o usuário digite -1 para parar. Ele deve validar a entrada para garantir que o que foi digitado é realmente um número antes de prosseguir. Apenas os números entre 0 até 100 devem ser considerados válidos e adicionados à lista. Ao final, imprima a soma dos números válidos e a lista dos números coletaods.
+#entrada: vários valores digitados pelo usuário, um de cada vez.
+#saída: a soma dos números válidos e a lista dos números coletados.
+
+Entradas: 10,50, abc, -5, 101, 20, -1
+Resultado esperado:
+Soma dos números válidos: 80
+Números coletados: [10,50,20]
