66abd0d2ccade19d0d638480e6cf647d19268f3e
CamilaLima03 <226563668+CamilaLima03@users.noreply.github.com>
2025-09-03 15:48:33 -0300

    anuncio

---
+++ ex05.py
@@ -0,0 +1,24 @@
+#analise de notas
+#Você tem uma lista de tuplas (aluno, notas)
+#1-imprima a maior nota
+#2 - nomes alunos maiores notas
+#3 - nome alunos nota menor q 7
+
+notas = [("Ana", 9.5),
+         ("João", 8.0),
+         ("Maria", 10.0),
+         ("Pedro", 7.5),
+         ("Ana", 10.0),
+         ("Carlos", 6.5)
+]
+
+media = set(notas)
+maiornota = set()
+menornota = set()
+avaliacao = set()
+
+for aluno, notas in media:
+    
+
+
+
