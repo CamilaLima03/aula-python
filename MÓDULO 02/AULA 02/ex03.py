ad05376fe61ed0d23b1553fb188f0a579fe24ef8
CamilaLima03 <226563668+CamilaLima03@users.noreply.github.com>
2025-09-03 14:50:21 -0300

    ex03

---
+++ MÓDULO 02/AULA 02/ex03.py
@@ -0,0 +1,23 @@
+#inventário cada tupla representa uma lista (nome_do_produto,id)
+#estoque_principal:produtos dispóniveis loja
+#estoque online: produtos no site
+
+#imprimir conjuntos
+#1-disponiveis em ambos 
+#2-disp loja
+#3- dispo online
+
+estoque_principal = [("Camiseta", 101),
+                     ("Calça", 102),
+                     ("Boné", 103),
+                     ("Tênis", 104)
+]
+estoque_online = [("Boné", 103),
+                  ("Camisa Polo", 105)
+                  ("Calça", 102)
+                  ("Chinelo", 106)
+]
+disponivelnaloja = list()
+disponivelnosite = list()
+disponivelemambos = list()
+
